91746fec759f1069497869f9e355204cff0f8a4d
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2024-08-31 11:48:43 +0900

    maka chart作成

--- js/index.js
+++ js/index.js
@@ -14,6 +14,8 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+
+//firebaseの登録場所を指定
 const dbRef = ref(db, "household-log");
 
 // HTML要素
@@ -26,34 +28,7 @@ const recordListALL = document.getElementById('all-area');
 const recordListMinus = document.getElementById('minus-area');
 const recordListPlus = document.getElementById('plus-area');
 
-// Chart の初期化
-let chartLabels = ["サンプルA", "サンプルB", "サンプルC", "サンプルD", "サンプルF"];
-let chartDataList = [47, 25, 10, 65, 52, 89]; // 数値の配列に修正
-
-var ctx = document.getElementById("myChart").getContext("2d");
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: chartLabels,
-        datasets: [
-            {
-                label: "お金の増減表",
-                data: chartDataList,
-                tension: 0,
-                fill: true,
-                borderWidth: 2,
-                borderColor: 'rgba(61, 51, 65, 1)',
-                backgroundColor: 'rgba(188, 178, 201, 0.2)'
-            }
-        ]
-    },
-    options: {
-        scales: {
-            x: {beginAtZero: true},
-            y: {beginAtZero: true}
-        }
-    }
-});
+makeChart();
 
 // `recordButton` のクリックイベントリスナー
 recordButton.addEventListener('click', () => {
